cf78c8cbd99f1726090c6d0481c9915ca1b18a02
raphaelle.roffo <raphaelle.roffo@gmail.com>
2021-03-23 22:21:59 +0100

    fix sqlite path

--- etl/0.create_sql_database.py
+++ etl/0.create_sql_database.py
@@ -1,5 +1,5 @@
 import sqlalchemy as sa
 import pandas as pd
 
-e = sa.create_engine("sqlite:///raw_data.sqlite")
+e = sa.create_engine("sqlite:///data/sql/raw_data.sqlite")
 pd.read_csv("data/raw/applications.csv").to_sql("raw_data", e)
