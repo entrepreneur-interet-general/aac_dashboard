7ec9ba1a92f95984dbbe0d66170ee8e6172bf2bc
raphaelle.roffo <raphaelle.roffo@gmail.com>
2021-04-28 14:13:43 +0200

    added todo into sql import worklfow

--- etl/0.create_sql_database.py
+++ etl/0.create_sql_database.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 import pandas as pd
+import sqlite3
 
 xl = pd.ExcelFile("data/raw/applications.xlsx")
 res = len(xl.sheet_names)
@@ -8,3 +9,7 @@ e = sa.create_engine("sqlite:///data/sql/raw_data.sqlite")
 for i in range(0,res-1):
         pd.read_excel("data/raw/applications.xlsx", sheet_name=i).to_sql("application_tab_%s" %i, e)
 
+# TODO : Need to build SQL table with shorter names to import csv into ###########
+# 
+# conn = sqlite3.connect('./data/sql/raw_data.sqlite')
+# c = conn.cursor()
